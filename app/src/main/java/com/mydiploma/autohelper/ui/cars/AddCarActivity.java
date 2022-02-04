b546124623694bec7abcf9daea344a8334cd9fec
lilpaan <bobmarlty777@gmail.com>
2022-02-04 15:07:39 +0300

    add fields in car add activity

--- app/src/main/java/com/mydiploma/autohelper/ui/cars/AddCarActivity.java
+++ app/src/main/java/com/mydiploma/autohelper/ui/cars/AddCarActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.mydiploma.autohelper.Car;
 import com.mydiploma.autohelper.R;
 
 public class AddCarActivity extends AppCompatActivity {
@@ -16,7 +19,13 @@ public class AddCarActivity extends AppCompatActivity {
         Button ok = findViewById(R.id.saveCarButton);
         Button cancelAdd = findViewById(R.id.cancelAddCarButton);
         ok.setOnClickListener(v -> {
-
+/*            Car car = new Car();
+            EditText edit = (EditText)findViewById(R.id.inputMaker);
+            TextView tview = (TextView)findViewById(R.id.inputMaker);
+            String result = edit.getText().toString();
+            tview.setText(result);*/
+/*            car.setMaker(findViewById(R.id.inputMaker).toString());
+            car.setModel();*/
         });
         cancelAdd.setOnClickListener(v -> {
             finish();
