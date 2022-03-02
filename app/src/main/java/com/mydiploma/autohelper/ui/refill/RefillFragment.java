c0c78b160cc09170b3e3444d403f481e571d72e0
lilpaan <bobmarlty777@gmail.com>
2022-03-02 20:14:18 +0300

    work with maps

--- app/src/main/java/com/mydiploma/autohelper/ui/refill/RefillFragment.java
+++ app/src/main/java/com/mydiploma/autohelper/ui/refill/RefillFragment.java
@@ -1,6 +1,7 @@
 package com.mydiploma.autohelper.ui.refill;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,32 +9,49 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.mydiploma.autohelper.Constants;
 import com.mydiploma.autohelper.R;
 import com.mydiploma.autohelper.databinding.FragmentNotificationsBinding;
 import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.location.Location;
+import com.yandex.mapkit.location.LocationListener;
+import com.yandex.mapkit.location.LocationStatus;
 import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.mapview.MapView;
 
 public class RefillFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    MapView mapview;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MapKitFactory.initialize(requireActivity());
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
         // Укажите имя activity вместо map.
-        MapView mapview;
-        mapview = root.findViewById(R.id.map_view);
-        mapview.getMap().move(
-                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
-                new Animation(Animation.Type.SMOOTH, 0),
-                null);
+        mapview = root.findViewById(R.id.mapview);
+        MapKit mapKit = MapKitFactory.getInstance();
+
+        mapKit.createLocationManager().requestSingleUpdate(new LocationListener() {
+            @Override
+            public void onLocationUpdated(@NonNull Location location) {
+                Log.d("TagCheck", "LocationUpdated " + location.getPosition().getLongitude());
+                Log.d("TagCheck", "LocationUpdated " + location.getPosition().getLatitude());
+                mapview.getMap().move(
+                        new CameraPosition(location.getPosition(), 14.0f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, 1),
+                        null);
+
+            }
+
+            @Override
+            public void onLocationStatusUpdated(@NonNull LocationStatus locationStatus) {
+
+            }
+        });
         return root;
     }
 
@@ -41,5 +59,19 @@ public class RefillFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapview.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapview.onStart();
+        MapKitFactory.getInstance().onStart();
     }
 }
