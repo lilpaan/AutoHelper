46604bb7a088ea70050d7b426ed6345d424d5fda
lilpaan <bobmarlty777@gmail.com>
2022-03-07 22:12:22 +0300

    car list is empty teper' krasivoe!

---
+++ app/src/test/java/com/mydiploma/autohelper/AutoHelperUnitTest.java
@@ -0,0 +1,109 @@
+package com.mydiploma.autohelper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AutoHelperUnitTest {
+
+    @Test
+    public void addNewCarPositive() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void addSparePartPositive() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void addInfoCardPositive() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void addNewCarNegative() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void addSparePartNegative() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void addInfoCardNegative() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showRefill() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showCars() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showBusinessCards() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showDiscountCards() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showSpareParts() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showCarInfo() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showInfoCardInfo() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showSparePartInfo() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void deleteCar() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void deleteSparePart() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void showCarListIsEmpty() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getCarCount() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getBusinessCardCount() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getDiscountCardCount() {
+        assertTrue(true);
+    }
+
+}
